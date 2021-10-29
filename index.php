885aebc2aac8528d0c743b465b3fed5d51f4b1a1
tekke <tekken5953@naver.com>
2021-10-29 14:45:48 +0900

    ss

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE HTML>
-
 <html>
 	<head>
 		<title>Portfolio</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link href="assets/css/main.css" />
 	</head>
 	<body class="is-preload">
 
